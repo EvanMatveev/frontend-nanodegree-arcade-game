37fc83d072d34d8fcec1c359b72e078c5b831dcd
Ivan Matveev <evanmatveev@gmail.com>
2018-08-14 12:33:32 +0300

    refactor: move methods in prototype into class declaration

--- js/app.js
+++ js/app.js
@@ -6,25 +6,23 @@ class Enemy {
     this.speed = speed;
     this.sprite = "images/enemy-bug.png";
   }
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {}
+  // Draw the enemy on the screen, required method for game
+  render(dt) {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-};
+// You should multiply any movement by the dt parameter
+// which will ensure the game runs at the same speed for
+// all computers.
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+Enemy.prototype.render = function() {};
 
 // Now write your own player class
 // This class requires an update(), render() and
